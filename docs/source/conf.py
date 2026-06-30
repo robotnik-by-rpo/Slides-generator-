871ba55cb79bcc697349f53ac6d25185fa27eff8
Monke <chikindin99@inbox.ru>
2026-07-01 00:44:51 +0800

    Added sphinx documentation for developers

---
+++ docs/source/conf.py
@@ -0,0 +1,92 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+from datetime import datetime
+
+# Добавляем путь к исходникам проекта (относительно папки source)
+# Поднимаемся на 2 уровня вверх: source -> docs -> корень проекта
+sys.path.insert(0, os.path.abspath('../../src'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Slide Deck Generator'
+copyright = f'2026, Your Org'
+author = 'Your Org'
+
+version = '1.0'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',          
+    'sphinx.ext.napoleon',        
+    'sphinx.ext.viewcode',        
+    'sphinx.ext.todo',             
+    'sphinx.ext.coverage',         
+    'sphinx.ext.intersphinx',      
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'ru'
+
+# -- Options for autodoc ----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
+
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__',
+}
+
+# -- Options for napoleon ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = True
+napoleon_use_ivar = True
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'navigation_depth': 4,
+    'titles_only': False,
+}
+
+html_static_path = ['_static']
+
+# -- Options for intersphinx ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'requests': ('https://requests.readthedocs.io/en/latest/', None),
+    'pytest': ('https://docs.pytest.org/en/latest/', None),
+}
+
+# -- Options for todo --------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html
+
+todo_include_todos = True
---
+++ docs/source/index.rst
@@ -0,0 +1,117 @@
+.. Slide Deck Generator documentation master file, created by
+   sphinx-quickstart on Wed Jul  1 00:32:18 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Slide Deck Generator documentation
+==================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+Автоматическая генерация презентаций из Markdown с использованием AI.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Содержание
+
+   📖 Руководство пользователя <user_guide>
+   🔧 Документация разработчика <developer_guide>
+   📚 API Reference <api_reference>
+
+Руководство пользователя
+========================
+
+Быстрый старт
+-------------
+
+.. code-block:: bash
+
+   # Генерация презентации
+   python -m src.__main__ --plan lesson.md --format pdf
+
+   # Генерация всех форматов
+   python -m src.__main__ --plan lesson.md --format all
+
+   # Обновление существующей
+   python -m src.__main__ --update ./output
+
+Переменные окружения
+--------------------
+
+.. code-block:: bash
+
+   # Обязательные
+   API_AI=your_groq_api_key
+   SLIDES_OUTPUT_FOLDER=/path/to/output
+
+   # Опциональные
+   THEME_MARP=default
+   LRS_ENDPOINT=http://localhost:8000/xAPI/statements
+   API_NEXTCLOUD=http://localhost:8080
+
+Документация разработчика
+=========================
+
+Архитектура
+-----------
+
+Проект состоит из 4 основных модулей:
+
+1. **parse** - Обработка Markdown и AI генерация
+2. **generate** - Конвертация в PDF/PPTX/HTML
+3. **metadata** - LRS и NextCloud интеграция
+4. **cli** - Интерфейс командной строки
+
+Установка для разработки
+------------------------
+
+.. code-block:: bash
+
+   git clone <repo>
+   cd slide_deck_generator
+   python -m venv venv
+   source venv/bin/activate
+   pip install -e .
+   pip install -r requirements.txt
+
+Запуск тестов
+-------------
+
+.. code-block:: bash
+
+   pytest
+   pytest --cov=src tests/
+
+API Reference
+=============
+
+Модуль generate
+---------------
+
+.. automodule:: generate.generate
+   :members:
+   :undoc-members:
+
+Модуль metadata
+---------------
+
+.. automodule:: metadata.metadata
+   :members:
+   :undoc-members:
+
+Модуль parse
+------------
+
+.. automodule:: parse.parse
+   :members:
+   :undoc-members:
+
+Модуль CLI
+----------
+
+.. automodule:: src.__main__
+   :members:
+   :undoc-members:
